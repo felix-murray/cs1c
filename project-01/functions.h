8eb8517d0382bb8ba4ce1427401932b69f64b796
Felix <felix.murray@gmail.com>
2019-12-24 15:58:07 -0600

    [feat] Add functions.cpp, numnbers.txt

---
+++ project-01/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+void pause();
+void populateArray(int list[], const int LENGTH);
+void print(int array[], const int LENGTH);
+void readFile(int list[], const int LENGTH);
+void writeFile(int list[], const int LENGTH);
+void selectionSort(int list[], const int LENGTH);
+int selectNum(int list[], const int LENGTH);
+void menu(int list[], const int LENGTH);
+
+#endif
