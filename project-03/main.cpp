56324807a3855d3a2838a1330475bae6f0336aef
Felix <felix.murray@gmail.com>
2019-12-25 22:18:02 -0600

    [feat] Add 'programmer' object definition.

--- project-03/main.cpp
+++ project-03/main.cpp
@@ -8,9 +8,15 @@ int main()
 {
     employee test;
     employee first("Felix", "1111499", "949-690-7443", 19, 'M', "Software Developer", 1000);
+    programmer bybel;
+    programmer walters("Grant Walters", "5678746", "714-588-1555", 21, 'F', "Web Engineer", 50000, 34, "Tylero", 4.5, false, true);
 
     system("clear");
     test.printEmployee();
     std::cout << "\n\n";
     first.printEmployee();
+    std::cout << "\n\n";
+    bybel.printProgrammer();
+    std::cout << "\n\n";
+    walters.printProgrammer();
 }
---
+++ project-03/programmer.h
@@ -0,0 +1,54 @@
+#ifndef PROGRAMMER_H_
+#define PROGRAMMER_H_
+
+#include <string>
+ 
+class programmer : public employee
+{
+    public:
+        /* GETTERS */
+        int getDepartmentNumber();
+        std::string getSupervisor();
+        double getSalaryIncreasePercentage();
+        bool getKnowsCPP();
+        bool getKnowsJava();
+
+        /* SETTERS */
+        void setDepartmentNumber(int initDepartmentNumber);
+        void setSupervisor(std::string initSupervisor);
+        void setSalaryIncreasePercentage(double initSalaryIncreasePercentage);
+        void setKnowsCPP(bool initKnowsCPP);
+        void setKnowsJava(bool initKnowsJava);
+        void setAll(int initDepartmentNumber, 
+                    std::string initSupervisor, 
+                    double initSalaryIncreasePercentage,
+                    bool initKnowsCPP,
+                    bool initKnowsJava);
+
+        /* CONSTRUCTORS */
+        programmer();
+        programmer(std::string initName, 
+                   std::string initID,
+                   std::string initPhoneNumber, 
+                   int initAge,
+                   char initGender,
+                   std::string initJobTitle,
+                   int initSalary,
+                   int initDepartmentNumber, 
+                   std::string initSupervisor, 
+                   double initSalaryIncreasePercentage,
+                   bool initKnowsCPP,
+                   bool initKnowsJava);
+
+        /* METHODS */
+        void printProgrammer();
+
+    private:
+        int departmentNumber;
+        std::string supervisor;
+        double salaryIncreasePercentage;
+        bool knowsCPP;
+        bool knowsJava;
+};
+
+#endif
