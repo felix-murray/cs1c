a0adb3be8335bec5c5d406e1694f82d1896a2622
Felix <felix.murray@gmail.com>
2020-01-06 22:35:00 -0600

    [feat] Implement reverse() function using recursion.

---
+++ project-06/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include <string>
+
+void reverse(std::string toReverse, int first, int last);
+void getInput(std::string toReverse);
+
+#endif
--- project-06/main.cpp
+++ project-06/main.cpp
@@ -1,48 +1,15 @@
 #include <iostream>
 #include <string>
-
-
-void iterative(char str[], int first, int last)
-{
-    int length = last - first + 1;
-    int tempLength = length;
-    char temp[length];
-    char reversed[length];
-
-    for (int i = first; i < last; i++)
-    {
-        temp[i] = str[i];
-    }
-
-    for (int i = 0; i < length; i++)
-    {
-        std::cout << temp[i];
-    } 
-
-    for (int i = 0; i < length; i++)
-    {
-        reversed[i] = temp[tempLength];
-        tempLength--;
-    } 
-
-    // for (int i = 0; i < length; i++)
-    // {
-    //     std::cout << reversed[i];
-    // } 
-}
+#include "functions.h"
 
 int main()
 {
-    char alphabet[] = {'a', 'b', 'c', 'd', 
-                       'e', 'f', 'g', 'h', 
-                       'i', 'j', 'k', 'l', 
-                       'm', 'n', 'o', 'p', 
-                       'q', 'r', 's', 't', 
-                       'u', 'v', 'w', 'x', 
-                       'y', 'z'};
+    system("clear");
+    
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    std::cout << alphabet[11];                      
-    //iterative(alphabet, 11, 18);
+    /* Tested with values: (11,18); (4,22); (0,25) */
+    getInput(alphabet);
 
     return 0;
 }
