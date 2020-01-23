fb8d82033556e96779e12a5f552c67911bd57deb
Felix <felix.murray@gmail.com>
2020-01-23 13:23:39 -0800

    [feat] Add documentation.

--- project-01/main.cpp
+++ project-01/main.cpp
@@ -1,3 +1,21 @@
+//--------------------------------------------------------------------------
+//  Project name: HW01
+//--------------------------------------------------------------------------
+//  Creator's name and email: Felix Murray felix.murray@gmail.com
+//  Course Selection: CS1C
+//  Due Date: 1/23/2020
+//--------------------------------------------------------------------------
+//  Purpose: Demonstrate knowledge of previously learned concepts from CS1B
+//           including arrays, sorting, enumerated data types, strings, and 
+//           file I/O using an array of randomly generated numbers.
+//--------------------------------------------------------------------------
+//  Algorithm:
+//      Step 1: Clear console and initialize array.
+//      Step 2: Display menu and await user selection.
+//       ..
+//       ..
+//      Step 3: Exit (when selected by the user)
+//--------------------------------------------------------------------------
 #include "functions.h"
 
 int main() 
